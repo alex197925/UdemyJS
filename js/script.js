0348406f9177fed63aba557b1a1d9f45aee6d7f8
Alex <tolok.oleksandr@gmail.com>
2020-05-13 17:57:43 +0200

    javascript Udemy

--- js/script.js
+++ js/script.js
@@ -14,22 +14,92 @@
 // alert(`privet, ${ user }`);
 
 
-const namberOfMovies = +prompt('How many movies did you see?', '');
+// const namberOfMovies = +prompt('How many movies did you see?', '');
 
-const personalMovieDB = {
-	count: namberOfMovies,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
-};
+// const personalMovieDB = {
+// 	count: namberOfMovies,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false
+// };
 
-const a = prompt('Whitch last movie you saw?', ''),
-	b = prompt('How good was movie?', ''),
-	c = prompt('Whitch last movie you saw?', ''),
-	d = prompt('How good was movie', '');
+// const a = prompt('Whitch last movie you saw?', ''),
+// 	b = prompt('How good was movie?', ''),
+// 	c = prompt('Whitch last movie you saw?', ''),
+// 	d = prompt('How good was movie', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
+
+
+// Ucloviya-------------------------------------------------
+
+// if (4 == 9) {
+// 	console.log('ok');
+// } else {
+// 	console.log('error');
+// }
+
+
+// const num = 50;
+
+
+// if (num < 49) {
+// 	console.log('Error');
+// } else if (num > 100) {
+// 	console.assert.log('To much');
+// } else {
+// 	console.log('ok');
+// }
+
+// -------  SWITCH  --------------
+
+// const num = 50;
+
+// switch (num) {
+// 	case 49:
+// 		console.log('Neverno');
+// 		break;
+// 	case 100:
+// 		console.log('Neverno');
+// 		break;
+// 	case 50:
+// 		console.log('Pravda');
+// 		break;
+// 	default:
+// 		console.log('Ne v etot raz');
+// 		break;
+
+// }
+
+
+// ------ CIKLI ------------
+// -Perviy Cpocob-
+
+//let num = 50;
+
+// while (num <= 55) {
+// 	console.log(num);
+// 	num++;
+// }
+
+//---- Vtoroy cpocob----
+// let num = 50;
+
+// do {
+// 	console.log(num);
+// 	num++;
+// }
+// while (num < 55);
+
+// --- Tretiy Camiy icpolzuuemiy ------
+
+let num = 50;
+
+for (let i = 1; i < 9; i++) {
+	console.log(num);
+	num++;
+}
